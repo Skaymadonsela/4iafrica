b41ca5dc45ebddd2da87a353913b4b80c96704d4
Skaymadonsela <madonsela.skay@gmail.com>
2023-08-08 11:46:56 +0200

    Added more files as part of my experiment

--- js/modal.js
+++ js/modal.js
@@ -8,78 +8,287 @@ const openButton1 = document.querySelector("#openModal1");
 
 const myModal = document.querySelector("#myModal1");
 
-const modalContent1 = document.querySelector("#modalContent1");
-
-const backCover1 = document.querySelector("#backCover1")
-
-const modalHeader = document.querySelector("#modalTitle1");
-
-const modalText1 = document.querySelector("#modalText1");
-
-const closeButton = document.querySelector("#close1");
-
-const greenEarthBtn1 = document.querySelector("#greenEarthBtn1");
-
-const frontImage1 = document.querySelector("#frontImage1");
+const closeBtn = document.querySelector("#close1");
 
 
-
-openButton.addEventListener("click", () => {
+// Open modal
+openButton1.addEventListener("click", () => {
   myModal.style.display = "block";
-  // modalText1.style.display = "block";
-  frontImage1.style.display = "block";
-  closeButton.style.display = "block";
 });
 
-greenEarthBtn1.addEventListener("click", ()=> {
-  backCover1.style.display = "Block"
-  frontImage1.style.display = "none"
-  greenEarthBtn1.style.display = "none"
-});
 
-closeButton.addEventListener("click", () => {
+// Close modal
+
+closeBtn.addEventListener("click", () => {
+  
   myModal.style.display = "none";
-  modalText1.style.display = "none";
-  modalHeader.style.display = "none";
-  closeButton.style.display = "none";
 });
 
-// Modal 2 
+// Modal 2
 
-const openModal2 = document.querySelector("#openModal2");
+
+
+const openButton2 = document.querySelector("#openModal2");
 
 const myModal2 = document.querySelector("#myModal2");
 
-const modalText2 = document.querySelector("#modalText2");
+const closeBtn2 = document.querySelector("#close2");
 
-const closeButton2 = document.querySelector("#close2");
 
-const greenEarthBtn2 = document.querySelector("#greenEarthBtn2");
-
-const frontImage2 = document.querySelector("#frontImage2")
-
-const backCover2 = document.querySelector("#backCover2")
-
-openModal2.addEventListener("click", () => {
+// Open modal
+openButton2.addEventListener("click", () => {
   myModal2.style.display = "block";
-  modalText2.style.display = "block";
-  modalHeader.style.display = "block";
-  closeButton2.style.display = "block";
+  closeBtn2.style.display = "block";
 });
 
 
-closeButton2.addEventListener("click", () => {
+// Close modal
+
+closeBtn2.addEventListener("click", () => {
+  
   myModal2.style.display = "none";
-  modalText2.style.display = "none";
-  modalHeader.style.display = "none";
-  closeButton2.style.display = "none";
 });
 
-greenEarthBtn2.addEventListener("click", ()=> {
-  backCover2.style.display = "Block"
-  frontImage2.style.display = "none"
-  greenEarthBtn2.style.display = "none"
-})
+
+// Modal 3
+
+const openButton3 = document.querySelector("#openModal3");
+
+const myModal3 = document.querySelector("#myModal3");
+
+const closeBtn3 = document.querySelector("#close13");
+
+
+// Open modal
+openButton3.addEventListener("click", () => {
+  myModal3.style.display = "block";
+  closeBtn3.style.display = "block";
+});
+
+
+// Close modal
+
+closeBtn3.addEventListener("click", () => {
+  
+  myModal3.style.display = "none";
+});
+
+
+// Modal 4
+
+const openButton4 = document.querySelector("#openModal4");
+
+const myModal4 = document.querySelector("#myModal4");
+
+const closeBtn4 = document.querySelector("#close14");
+
+
+// Open modal
+openButton4.addEventListener("click", () => {
+  myModal4.style.display = "block";
+  closeBtn4.style.display = "block";
+});
+
+
+// Close modal
+
+closeBtn4.addEventListener("click", () => {
+  
+  myModal4.style.display = "none";
+});
+
+
+// Modal 5
+
+const openButton5 = document.querySelector("#openModal5");
+
+const myModal5 = document.querySelector("#myModal5");
+
+const closeBtn5 = document.querySelector("#close15");
+
+
+// Open modal
+openButton5.addEventListener("click", () => {
+  myModal5.style.display = "block";
+  closeBtn5.style.display = "block";
+});
+
+
+// Close modal
+
+closeBtn5.addEventListener("click", () => {
+  
+  myModal5.style.display = "none";
+});
+
+
+// Modal 6
+
+const openButton6 = document.querySelector("#openModal6");
+
+const myModal6 = document.querySelector("#myModal6");
+
+const closeBtn6 = document.querySelector("#close16");
+
+
+// Open modal
+openButton6.addEventListener("click", () => {
+  myModal6.style.display = "block";
+  closeBtn6.style.display = "block";
+});
+
+
+// Close modal
+
+closeBtn6.addEventListener("click", () => {
+  
+  myModal6.style.display = "none";
+});
+
+
+// Modal 7
+
+const openButton7 = document.querySelector("#openModal7");
+
+const myModal7 = document.querySelector("#myModal7");
+
+const closeBtn7 = document.querySelector("#close17");
+
+
+// Open modal
+openButton7.addEventListener("click", () => {
+  myModal7.style.display = "block";
+  closeBtn7.style.display = "block";
+});
+
+
+// Close modal
+
+closeBtn7.addEventListener("click", () => {
+  
+  myModal7.style.display = "none";
+});
+
+// Modal 8
+
+const openButton8 = document.querySelector("#openModal8");
+
+const myModal8 = document.querySelector("#myModal8");
+
+const closeBtn8 = document.querySelector("#close18");
+
+
+// Open modal
+openButton8.addEventListener("click", () => {
+  myModal8.style.display = "block";
+  closeBtn8.style.display = "block";
+});
+
+
+// Close modal
+
+closeBtn8.addEventListener("click", () => {
+  
+  myModal8.style.display = "none";
+});
+
+
+// Modal 9
+
+const openButton9 = document.querySelector("#openModal9");
+
+const myModal9 = document.querySelector("#myModal9");
+
+const closeBtn9 = document.querySelector("#close19");
+
+
+// Open modal
+openButton9.addEventListener("click", () => {
+  myModal9.style.display = "block";
+  closeBtn9.style.display = "block";
+});
+
+
+// Close modal
+
+closeBtn9.addEventListener("click", () => {
+  
+  myModal9.style.display = "none";
+});
+
+
+// Modal 10
+
+const exploreBtn1 = document.querySelector("#red");
+
+const myModal10 = document.querySelector("#myModal10");
+
+const closeBtn10 = document.querySelector("#close10");
+
+// Open modal
+exploreBtn1.addEventListener("click", () => {
+  myModal10.style.display = "block";
+  closeBtn10.style.display = "block";
+});
+
+// Close modal
+
+closeBtn10.addEventListener("click", () => {
+  
+  myModal10.style.display = "none";
+});
+
+
+// Modal 11
+
+const exploreBtn2 = document.querySelector("#yellow");
+
+const myModal11 = document.querySelector("#myModal11");
+
+const closeBtn11 = document.querySelector("#close11");
+
+// Open modal
+exploreBtn2.addEventListener("click", () => {
+  myModal11.style.display = "block";
+  closeBtn11.style.display = "block";
+});
+
+// Close modal
+
+closeBtn11.addEventListener("click", () => {
+  
+  myModal11.style.display = "none";
+});
+
+
+
+// Modal 12
+
+const exploreBtn3 = document.querySelector("#green");
+
+const myModal12 = document.querySelector("#myModal12");
+
+const closeBtn12 = document.querySelector("#close12");
+
+// Open modal
+exploreBtn3.addEventListener("click", () => {
+  myModal12.style.display = "block";
+  closeBtn12.style.display = "block";
+});
+
+// Close modal
+
+closeBtn12.addEventListener("click", () => {
+  
+  myModal12.style.display = "none";
+});
+
+
+
+
+
+
+
 
 
 
